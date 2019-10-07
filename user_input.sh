cc34aa210f2e36fd5b32db6c96a06c00e12696f5
electricSamarth <samarthmathur13@gmail.com>
2019-10-08 01:49:13 +0530

    inline inpt, silent input, array input and display ,

--- user_input.sh
+++ user_input.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 echo Enter names:
-read name1 name2 name3
-echo  first name : $name1 middle name: $name2 Lastname:$name3
+read name1 
+echo  first name : $name1
+
+read -p "username : " user_var #same line input
+read -sp "password : " pass_var #same line silent input
+echo $user_var $pass_var
+
+echo "Enter array space seperated"
+read -a array_num
+echo "Numbers: ${array_num[0]} , ${array_num[1]} " 
+
+echo "enter with missing var name in read"
+read 
+echo "default is system variable REPLY is" $REPLY
